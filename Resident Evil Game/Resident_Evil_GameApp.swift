1a1c2fe353c9ff696b3020e527185a20497cefeb
Giorgio Giannotta <jorge_upa@msn.com>
2023-03-19 12:37:31 +0100

    Initial Commit

---
+++ Resident Evil Game/Resident_Evil_GameApp.swift
@@ -0,0 +1,17 @@
+//
+//  Resident_Evil_GameApp.swift
+//  Resident Evil Game
+//
+//  Created by Giorgio Giannotta on 19/03/23.
+//
+
+import SwiftUI
+
+@main
+struct Resident_Evil_GameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
